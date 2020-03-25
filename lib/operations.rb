6251df4adccc3e1d630ca52743e05a1d107ef5ef
Micah McCallum <micahmccallum@outlook.com>
2020-03-25 16:43:03 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+	if speed.between?(40, 60)
+		false
+	else
+		true
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed.between?(40, 60) ? false : true
+	binding.pry
 end
-	
-
-
