29bd6553a26ca9fffc931b31dbeb5afc636f3e7d
Micah McCallum <micahmccallum@outlook.com>
2020-03-25 16:43:41 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+	if speed.between?(40, 60)
+		false
+	else
+		true
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed.between?(40, 60) ? false : true
 end
-	
-
-
